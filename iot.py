0d0322837ccd1303c7c2cf278f8c58160efe551c
mustafa ashraf <mustafaashraf744@gmail.com>
2019-01-08 15:58:03 +0200

    Update iot.py

--- iot.py
+++ iot.py
@@ -14,13 +14,21 @@ def index():
 @route('/garagedoor/:doornum')
 def garagedoor(doornum=0):
  if doornum == '0':
+  print ("No door number specified")
   return 'No door number specified'
 
  elif doornum == '1':
+  print ("Door number 1 cycled")
   return 'Door number 1 cycled.'
-
+  
+  
  elif doornum == '2':
-  print("hiiiiiiiii")
+  print("Door number 2 cycled")
   return 'Door number 2 cycled'
 
+ elif doornum == '3':
+  print("Door number 3 cycled")
+  return 'Door number 3 cycled'
+
 run(host='0.0.0.0', port=1234)
+
