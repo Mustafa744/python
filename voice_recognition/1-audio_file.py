9a34df42354d4e0438e5a9592d314ab3894bdaf2
mustafa ashraf <mustafaashraf744@gmail.com>
2019-01-08 12:23:54 +0200

    Delete 1-audio_file.py

--- voice_recognition/1-audio_file.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-r = sr.Recognizer()
-harvard = sr.AudioFile('C:\harvard.wav')#file
-with harvard as source:
-    audio = r.record(source)
-
-type(audio)
-
-r.recognize_google(audio)
