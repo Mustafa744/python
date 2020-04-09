b1057bb182c0fb76c8329af38dca422d36ab537f
mustafa ashraf <mustafaashraf744@gmail.com>
2020-04-09 22:51:14 +0200

    Update mobile2.py

--- mobile2.py
+++ mobile2.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import urllib.request
 url = "http://192.168.1.3:8080/shot.jpg"
+drawing=1
 
 last_move = ""
 successful = 1
@@ -13,7 +14,7 @@ out = fourcc = 0
 
 state = ""
 ang = 0
-x_offset = 160
+x_offset = 120
 y_offset = 160
 leftmost=(0,0)
 rightmost=(319,0)
@@ -30,18 +31,17 @@ high_black = np.array([75, 75, 75])
 low_green = np.array([161, 155, 84])
 high_green = np.array([179, 255, 255])
 
-def convert_row_to_ascii(row):
-    # 17-long
-    ORDER = (' ', '.', "'", ',', ':', ';', 'c', 'l',
-             'x', 'o', 'k', 'X', 'd', 'O', '0', 'K', 'N')
-    return tuple(ORDER[int(x / (255 / 16))] for x in row)[::-1]
 
-def convert_to_ascii(input_grays):
-    return tuple(convert_row_to_ascii(row) for row in input_grays)
-
-def print_array(input_ascii_array):
-    os.system("clear")
-    print('\n'.join((''.join(row) for row in input_ascii_array)), end='')
+def draw_bar(position,length):
+    bar=[]
+    for i in range(length):
+        bar.append("_")
+    os.system('cls')
+    print("\n")
+    bar[int(position)-1]="O"
+    for char in bar:
+        print(char,end="")
+    bar[int(position)-1]="_"
 
 
 def correct_black(contours):
@@ -58,7 +58,7 @@ def correct_black(contours):
                 suspects.append([bottommost[1], index])
             suspects.sort()
             selection = suspects[-1][1]
-        cv2.drawContours(blank_image, contours_blk, selection, (255, 255, 255), 10)
+        #cv2.drawContours(blank_image, contours_blk, selection, (255, 255, 255), 10)
         cnt = contours_blk[selection]
 
         return cnt
@@ -88,9 +88,7 @@ def motor(speed, steering):
 
 
 
-#cap = cv2.VideoCapture(0)
-#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
 def correct_green(points):
     correct_points = []
     if points is not None:
@@ -114,9 +112,9 @@ def check_green():
     kernel = np.ones((3, 3), np.uint8)
     greenmark = cv2.erode(greenmark, kernel, iterations=3)
     greenmark = cv2.dilate(greenmark, kernel, iterations=9)
-    contours_green, hierarchy_green = cv2.findContours(greenmark, cv2.RETR_TREE,
+    __ ,contours_green, hierarchy_green = cv2.findContours(greenmark, cv2.RETR_TREE,
                                                                   cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(blank_image, contours_green, -1, (0, 255, 0), 10)
+
 
     if len(contours_green) > 2:
         contours_green = [contours_green[0], contours_green[1]]
@@ -126,7 +124,7 @@ def check_green():
         cy = int(M['m01'] / M['m00'])
         points.append((cx, cy))
     new_points = correct_green(points)
-    # cv2.imshow("green", blank_image2)"""
+
     return len(new_points), new_points
 
 
@@ -134,49 +132,32 @@ count = 0
 start = time.time()
 center = 160
 ang=0
-#ret, image_raw = cap.read()
+
+
 imgResp = urllib.request.urlopen(url)
 imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
 image_raw = cv2.imdecode(imgNp, -1)
 image_raw=image_raw[1:-1,1:-1]
 
 while successful:
-    # Get screensize for reduction
-    screen_height, screen_width = os.popen('stty size', 'r').read().split()
-    # Get screensize for reduction
-    #screen_height, screen_width = 120,120
-
     # black blank image
     blank_image = np.zeros(shape=[320, 320, 3], dtype=np.uint8)
-    #ret, image_raw = cap.read()
-
-    #image_raw = cv2.resize(image_raw, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
-
-
-    #image_raw = image_raw[0:400, :] #height width
-         #image_raw=rotateImage(image_raw,180)
     imgResp = urllib.request.urlopen(url)
     imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
     image_raw = cv2.imdecode(imgNp, -1)
 
 
-    #image_raw = image_raw[1:-1, 1:-1]
-    #print(image_raw.shape)
+
+
     count = count + 1
     image = np.array(image_raw)
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Reduce grayscale array to proper resolution
-    reduced = cv2.resize(gray, (int(screen_width), int(screen_height)))
-
-    # Plug in reduced resolution numpy array for ascii converter func
-    converted = convert_to_ascii(reduced)
-    print_array(converted)
+    #image = cv2.resize(image, None, fx=1.333, fy=1, interpolation=cv2.INTER_CUBIC)
 
     Blackline = cv2.inRange(image, low_black, high_black)
     Blackline = cv2.erode(Blackline, kernel, iterations=2)
     Blackline = cv2.dilate(Blackline, kernel, iterations=3)
-    contours_blk, hierarchy_blk = cv2.findContours(Blackline, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    __, contours_blk, hierarchy_blk = cv2.findContours(Blackline, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     line = correct_black(contours_blk)
     if line is not None:
@@ -186,7 +167,8 @@ while successful:
         M = cv2.moments(line)
         cx = int(M['m10'] / M['m00'])
         cy = int(M['m01'] / M['m00'])
-        #cv2.circle(image, (cx, cy), 10, (200, 0, 25), -1)
+
+
 
 
         leftmost = tuple(line[line[:, :, 0].argmin()][0])
@@ -207,24 +189,24 @@ while successful:
             ang = 90 + ang
         else:
             pass
-        error = int(x_min) - x_offset
+        error = int(cx) - x_offset
 
         if width >= x_offset * 2 - 3 or height >= y_offset * 2 - 3:
             length_green = check_green()
             if length_green[0] != 0:
-                #print("possible intersection")
+                print("possible intersection")
                 if length_green[0] ==2:
-                    #print("turn arround")
+                    print("turn arround")
                     ang=0
                     error=-250
                 else :
                     green_square=length_green[1][0]
                     if green_square[0]>cx :
-                        #print("intersection right")
+                        print("intersection right")
                         ang=0
                         error=190
                     elif green_square[0]<cx :
-                        #print("intersection left")
+                        print("intersection left")
                         ang = 0
                         error = -190
             else:
@@ -248,41 +230,24 @@ while successful:
 
         r, l = motor(100, error * factA + ang*factE)
 
-        #print(l, "   ", r)
+        #print(l, "   ", r,"  ", ang,"  ",error)
+        draw_bar(int(cx/5),50)
 
 
-        '''if r > 50 and l > 50:
-            #print("forward")
+"""if r > 50 and l > 50:
+            print("forward")
 
         elif r > l:
-            #print("left")
+            print("left")
         elif l > r:
-            #print("right")
-        #cv2.putText(image, str(l), (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.51, (0, 200, 0), 2)
-        #cv2.putText(image, str(r), (50, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 200, 0), 2)
+            print("right")"""
 
-        #cv2.putText(image, "ang "+str(ang), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        #cv2.putText(image, "eror "+str(error), (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 200, ), 2)
-        #cv2.circle(image, (error,120), 10, (200, 0, 250), -1)'''
 
-    else :
-        pass
-        #stop
-
-    if count>100:
+"""f count>500:
         finish=time.time()
-        break
-    #cv2.imshow("orginal with line", image)
-    #cv2.imshow("black", blank_image)
+        break"""
 
-''' key = cv2.waitKey(1) & 0xFF
-    if key == 27:
-        finish = time.time()
-        if successful == 2:
-            cap.release()
-        break
-        cv2.destroyAllWindows()'''
 
-fps = count/(finish - start)
-print("fps=  ", fps)
+#fps = count/(finish - start)
+#print("fps=  ", fps)
 exit()
